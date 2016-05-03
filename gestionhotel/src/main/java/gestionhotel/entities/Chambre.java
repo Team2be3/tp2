94e41e8f2f6e607802228b5a9cfc0a1293bb6e8b
INTI-0264 <INTI-0264@INTI0264>
2016-05-03 09:48:56 +0200

    Valérian - classes reservation et chambre

---
+++ gestionhotel/src/main/java/gestionhotel/entities/Chambre.java
@@ -0,0 +1,87 @@
+package gestionhotel.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+/*Auteur: Valérian THOMAS
+ * nom: gestionhotel
+ *date: 02/05/2016
+ *Package:gestionhotel.entities
+ *Class:Reservation
+ *version:1
+ *association: Reservation (ManyToMany)
+ * 
+ * */
+
+@Entity
+
+public class Chambre implements Serializable{
+	
+	//attributs
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long idChambre;
+	@Column
+	private Integer capacite;
+	@Column
+	private Double prix;
+	@Column
+	private String description;
+	
+	//association avec Reservation
+	
+	@ManyToMany (fetch=FetchType.LAZY)
+	@JoinTable(name="Chambre_res")
+	private List<Reservation> listereservation = new ArrayList<Reservation>();
+
+	//getters et setters
+	
+	public Long getIdChambre() {
+		return idChambre;
+	}
+	public void setIdChambre(Long idChambre) {
+		this.idChambre = idChambre;
+	}
+	public Integer getCapacite() {
+		return capacite;
+	}
+	public void setCapacite(Integer capacite) {
+		this.capacite = capacite;
+	}
+	public Double getPrix() {
+		return prix;
+	}
+	public void setPrix(Double prix) {
+		this.prix = prix;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	//constructeurs
+	
+	public Chambre(Integer capacite, Double prix, String description) {
+		super();
+		this.capacite = capacite;
+		this.prix = prix;
+		this.description = description;
+	}
+	
+	public Chambre() {
+		// TODO Auto-generated constructor stub
+	}
+	
+}
---
+++ gestionhotel/src/main/java/gestionhotel/entities/Reservation.java
@@ -0,0 +1,93 @@
+package gestionhotel.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+/*Auteur: Valérian THOMAS
+ * nom: gestionhotel
+ *date: 02/05/2016
+ *Package:gestionhotel.entities
+ *Class:Reservation
+ *version:1
+ *association: Employé (ManyToOne), Client (ManyToOne), Chambre (ManyToMany)
+ * 
+ * */
+
+
+@Entity
+public class Reservation implements Serializable {
+	
+	
+	//attributs
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long idReservation;
+	@Temporal(TemporalType.DATE)
+	private Date dateDebut;
+	@Temporal(TemporalType.DATE)
+	private Date dateFin;
+	@Column(name="Etat_Reservation")
+	private String etatReservation;
+	
+	
+	//association avec Chambre
+	@ManyToMany(fetch=FetchType.LAZY, mappedBy="listereservation")
+	private List<Chambre> listechambre=new ArrayList<Chambre>();
+
+	
+	//getters and setters
+	
+	public Long getIdReservation() {
+		return idReservation;
+	}
+	public void setIdReservation(Long idReservation) {
+		this.idReservation = idReservation;
+	}
+	public Date getDateDebut() {
+		return dateDebut;
+	}
+	public void setDateDebut(Date dateDebut) {
+		this.dateDebut = dateDebut;
+	}
+	public Date getDateFin() {
+		return dateFin;
+	}
+	public void setDateFin(Date dateFin) {
+		this.dateFin = dateFin;
+	}
+	public String getEtatReservation() {
+		return etatReservation;
+	}
+	public void setEtatReservation(String etatReservation) {
+		this.etatReservation = etatReservation;
+	}
+
+	
+	//constructeurs
+	
+	public Reservation(Date dateDebut, Date dateFin, String etatReservation) {
+		super();
+		this.dateDebut = dateDebut;
+		this.dateFin = dateFin;
+		this.etatReservation = etatReservation;
+	}
+		
+	public Reservation() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
