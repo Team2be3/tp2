37fe2ee8635c0d6b7207ae365802bbab1e3d5592
INTI-0257 <INTI-0257@INTI0257>
2016-05-03 12:27:48 +0200

    recommit dans le local

--- gestionhotel/src/main/java/gestionhotel/Metier/ImplChambreMetier.java
+++ gestionhotel/src/main/java/gestionhotel/Metier/ImplChambreMetier.java
@@ -1,5 +1,46 @@
 package gestionhotel.Metier;
 
-public class ImplChambreMetier {
+import gestionhotel.entities.Chambre;
+import gestionhotel.entities.Reservation;
+
+import java.util.List;
+
+public class ImplChambreMetier implements InterChambreMetier{
+
+	@Override
+	public void addchambre(Chambre c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletechambre(Long idChambre) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updatechambre(Chambre c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Chambre getChambre(Long idChambre) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Chambre> getlistechambres() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Reservation> getlistereservchambre(Long idChambre) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
