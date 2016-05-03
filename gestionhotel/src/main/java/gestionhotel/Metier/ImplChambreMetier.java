e25288230610d7ba16e9c988b56e094db0e3675e
INTI-0257 <INTI-0257@INTI0257>
2016-05-03 12:32:10 +0200

    sss

--- gestionhotel/src/main/java/gestionhotel/Metier/ImplChambreMetier.java
+++ gestionhotel/src/main/java/gestionhotel/Metier/ImplChambreMetier.java
@@ -1,17 +1,40 @@
 package gestionhotel.Metier;
 
+import gestionhotel.DAO.InterChambreDao;
 import gestionhotel.entities.Chambre;
 import gestionhotel.entities.Reservation;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 public class ImplChambreMetier implements InterChambreMetier{
+	
+	private final Logger LOG=Logger.getLogger("ImplemChambreMetier");
+	private InterChambreDao daochambre;
+	
+	
+	//setters
+	
+	public InterChambreDao getDaochambre() {
+		return daochambre;
+	}
+
+	public void setDaochambre(InterChambreDao daochambre) {
+		this.daochambre = daochambre;
+	}
+
+	public Logger getLOG() {
+		return LOG;
+	}
 
 	@Override
 	public void addchambre(Chambre c) {
 		// TODO Auto-generated method stub
 		
 	}
+
+	
 
 	@Override
 	public void deletechambre(Long idChambre) {
